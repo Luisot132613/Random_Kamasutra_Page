b5b540fc6414310ffff6b3c5d9b3f2a23538c1c6
Luisot132813 <luisotavioqueiroz37@gmail.com>
2026-09-15 22:00:07 -0300

    Comentários script.js

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ for (let i = 1; i <= 100; i++) {
   imagens.push(`./recortes/${i}.png`);
 }
 
-const botao = document.getElementById("mostrarImagem");
-const imagem = document.getElementById("imagemAleatoria");
+const botao = document.getElementById("mostrarImagem"); // Constante botão que retorna id=mostrarImagem no HTML
+const imagem = document.getElementById("imagemAleatoria"); // Constante imagem que retorna id=imagemAleatoria no HTML
 
+// Quando o botão é clicado -> gera um numero aleatório equivalente a lista do banco de imagens
+// Guarda o valor na const aleatório e muda o src da imagem p/ "{numero}.png -> buscando a imagem correta no .recortes"
 botao.addEventListener("click", () => {
   const aleatorio = Math.floor(Math.random() * imagens.length);
   imagem.src = imagens[aleatorio];
